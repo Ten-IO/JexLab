dcbed8e7ce4d1005100364895ff149e5fb6bb3b2
Ten_IO <194474940+Ten-IO@users.noreply.github.com>
2025-11-17 23:23:05 +0700

    add (ListJava): switch case, do while, ArrayList, sample

---
+++ week03/e04_inputStudent.java
@@ -0,0 +1,49 @@
+/*
+ * Write a program to allow user to infinitely input student to a table by using Array of Student Class. 
+ * You can suggest Attributes (id, name, age, etc.)
+ */
+package week03;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class e04_inputStudent {
+    static class Student {
+        int id, age;
+        String name;
+    }
+
+    public static void main(String[] args) {
+        boolean run = true;
+        int i = 0;
+        ArrayList<Student> list = new ArrayList<Student>();
+        try (Scanner scan = new Scanner(System.in)) {
+            do {
+                Student s = new Student();
+                System.out.printf("Student #%d:%nID: ", ++i);
+                s.id = scan.nextInt();
+                scan.nextLine();
+                System.out.print("Name: ");
+                s.name = scan.nextLine();
+                System.out.print("Age: ");
+                s.age = scan.nextInt();
+                list.add(s);
+                System.out.print("\nDo you want to add more (y/n)?: ");
+                String input = scan.next();
+                run = input.equalsIgnoreCase("y") ? true : false;
+                // If input is NOT 'n' and NOT 'y'
+                if (!input.equalsIgnoreCase("n") && !run)
+                    throw new IllegalArgumentException("Invalid input");
+            } while (run);
+            i = 0;
+            System.out.println(" _______________________________________");
+            System.out.printf("| %-2s | %-3s | %-20s | %-3s |%n", "No", "ID", "Name", "Age" );
+            System.out.println("|____|_____|______________________|_____|");
+            for (Student s: list)
+                System.out.printf("| %02d | %03d | %-20s | %3d |%n", ++i, s.id, s.name, s.age );
+            System.out.print("|____|_____|______________________|_____|");
+        } catch (Exception e) {
+            System.out.println("[!] Error: " + e.getClass().getSimpleName());
+        }
+    }
+}
